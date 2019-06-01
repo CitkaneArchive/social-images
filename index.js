400ed6a338f042509fc41b6173c5a90eca45a8c9
Michael Jonker <michael@openpoint.ie>
2019-06-02 00:19:42 +0100

    cleanup

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const zmq = require('zmq');
 const reqResPort = 4000;
 
 function makeResponder() {
-  const responder = zmq.socket('router');
-  responder.bindSync(`tcp://127.0.0.1:${reqResPort}`);
+    const responder = zmq.socket('router');
+    responder.bindSync(`tcp://127.0.0.1:${reqResPort}`);
 
-  responder.on('message', (...args) => {
-    const identity = args[0];
-    const message = args[2].toString();
-    console.log(message);
-    setTimeout(() => {
-      responder.send([identity, '', `response: ${message}`]);
-    }, 1000);
-  });
+    responder.on('message', (...args) => {
+        const identity = args[0];
+        const message = args[2].toString();
+        console.log(message);
+        setTimeout(() => {
+            responder.send([identity, '', `response: ${message}`]);
+        }, 1000);
+    });
 }
 
 makeResponder();
@@ -26,5 +26,5 @@ makeResponder();
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 http.listen(3010, () => {
-  console.log('listening on *:3010');
+    console.log('listening on *:3010');
 });
