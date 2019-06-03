552bbb7f17e1a85db3e01984627a465b7cbd52b4
Michael Jonker <michael@openpoint.ie>
2019-06-03 20:58:04 +0100

    add user file store

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+/* eslint-disable import/order */
+/* eslint-disable prefer-promise-reject-errors */
+const path = require('path');
+const express = require('express');
+const ports = require('@social/social-deployment/topology/portMaps');
+const Sockets = require('@social/social-deployment/templates/nodejs/api/Sockets');
+const Api = require('./src/api/Api');
+
+const app = express();
+const http = require('http').createServer(app);
+
+const sockets = new Sockets('images');
+
+sockets.subscribe('users.refreshCache');
+sockets.subscriber.on('message', (topic, message) => {
+    console.log('received a message related to:', topic.toString(), 'containing message:', message.toString());
+});
+setTimeout(() => {
+    sockets.publish('users.newUser', 'testing pubsub');
+}, 5000);
+
+const api = new Api(sockets);
+const apiInterface = {
+    create: {},
+    read: {
+        test: (ownerId, args) => api.test(...args)
+            .then(payload => ({ status: 201, payload }))
+            .catch(err => Promise.reject({ status: err.status || 500, message: err.message }))
+    },
+    update: {},
+    delete: {}
+};
+
+sockets.makeResponder(apiInterface);
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+http.listen(ports.images.static);
+
+/*
 const path = require('path');
 const express = require('express');
 const ports = require('@social/social-deployment/topology/portMaps');
@@ -49,6 +89,6 @@ makeResponder();
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-http.listen(3010, () => {
-    console.log('listening on *:3010');
-});
+http.listen(ports.images.static);
+
+*/
---
+++ src/api/Api.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-useless-constructor */
+/* eslint-disable class-methods-use-this */
+const uniqid = require('uniqid');
+const BaseApi = require('@social/social-deployment/templates/nodejs/api/BaseApi');
+
+class Api extends BaseApi {
+    constructor(sockets) {
+        super(sockets);
+    }
+
+    test() {
+        console.log(this);
+    }
+}
+
+module.exports = Api;
