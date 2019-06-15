1698ae9c1a4d0e5697166bfb0f02d60abe790094
Michael Jonker <michael@openpoint.ie>
2019-06-15 21:10:36 +0100

    add activities service

--- src/images.js
+++ src/images.js
@@ -1,19 +1,15 @@
 /* eslint-disable import/order */
 /* eslint-disable prefer-promise-reject-errors */
 const config = require('config');
-
-// eslint-disable-next-line no-underscore-dangle
-global.__network = config.get('network');
-
 const path = require('path');
 const express = require('express');
-const ports = require('@social/social-deployment/topology/portMaps');
 const Sockets = require('../../social-deployment/templates/nodejs/api/Sockets');
 const Api = require('./api/Api');
 
 const app = express();
 const http = require('http').createServer(app);
 
+const network = config.get('network');
 const sockets = new Sockets('images');
 
 sockets.subscribe('users.refreshCache');
@@ -40,60 +36,4 @@ sockets.makeResponder(apiInterface);
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-http.listen(ports.images.static, __network.host);
-
-/*
-const path = require('path');
-const express = require('express');
-const ports = require('@social/social-deployment/topology/portMaps');
-
-const app = express();
-const http = require('http').createServer(app);
-const zmq = require('zmq');
-
-const publisher = zmq.socket('push');
-publisher.bindSync(`tcp://127.0.0.1:${ports.images.pubsub}`);
-
-const subscriber = zmq.socket('sub');
-subscriber.connect(`tcp://127.0.0.1:${ports.pubsub}`);
-subscriber.subscribe('kitty.kat');
-subscriber.subscribe('doggy.dog');
-subscriber.on('message', (topic, message) => {
-    console.log('received a message related to:', topic.toString(), 'containing message:', message.toString());
-});
-
-function publish(topic, data) {
-    try {
-        const m = JSON.stringify([topic, data]);
-        publisher.send(m);
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-function makeResponder() {
-    const responder = zmq.socket('router');
-    responder.bindSync(`tcp://127.0.0.1:${ports.images.crud}`);
-
-    responder.on('message', (...args) => {
-        const identity = args[0];
-        // const message = args[2].toString();
-        const response = JSON.stringify({
-            status: 200,
-            payload: 'I did something in the images microservice'
-        });
-        setTimeout(() => {
-            publish('kitty.kat', 'Publish something in the images microservice');
-            publish('doggy.dog', 'Publish something in the images microservice2');
-            responder.send([identity, '', response]);
-        }, 100);
-    });
-}
-
-makeResponder();
-
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-http.listen(ports.images.static);
-
-*/
+http.listen(ports.images.static, network.host);
